fd8c3eb55e24b7112b4a15ff2d9b2496bdc10f28
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-11-10 22:25:11 +0900

    doc: value

---
+++ Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+// Swiftの変数の宣言
+// var 変数名: 型名
+
+// Swiftの定数の宣言
+// let 定数名: 型名
+
+// 通常の通常のInt型
+let a: Int = 1
+
+// nilを許容するInt型
+let b: Int? = nil
+
+// 通常のString型
+let c: String = "Hello"
+
+// コンパイルエラーになる例
+let d: Int = 123
+//let e: Int = "Hello"
---
+++ Sources/Value.swift
@@ -0,0 +1,56 @@
+import UIKit
+
+// Swiftの変数の宣言
+// var 変数名: 型名
+
+// Swiftの定数の宣言
+// let 定数名: 型名
+
+// 通常の通常のInt型
+let a: Int = 1
+
+// nilを許容するInt型
+let b: Int? = nil
+
+// 通常のString型
+let c: String = "Hello"
+
+// コンパイルエラーになる例
+let d: Int = 123
+let e: Int = "Hello"
+
+// データ型の種類
+// Int, UInt, Float, Double, Bool, String, Character, Array, Dictionary, Set, Optional
+
+// Int型
+let int1: Int = 1
+
+// UInt型
+let uint1: UInt = 1
+
+// Float型
+let float1: Float = 1.0
+
+// Double型
+let double1: Double = 1.0
+
+// Bool型
+let bool1: Bool = true
+
+// String型
+let string1: String = "Hello"
+
+// Character型
+let character1: Character = "A"
+
+// Array型
+let array1: Array<Int> = [1, 2, 3]
+
+// Dictionary型
+let dictionary1: Dictionary<String, Int> = ["A": 1, "B": 2, "C": 3]
+
+// Set型
+let set1: Set<Int> = [1, 2, 3]
+
+// Optional型
+let optional1: Int? = nil
